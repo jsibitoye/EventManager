e51091b6b2a9fc1e6630a13276f6b2e44361173b
J.s Ibitoye <Joshua4uonline@gmail.com>
2022-03-24 02:09:57 +0100

    commit

--- joshua.pas
+++ joshua.pas
@@ -29,7 +29,7 @@ procedure Tjoshuaform.Button1Click(Sender: TObject);
 begin
 try
   query.Close;
-  query.SQL.Text:='insert into staff(name) values("joshua")';
+  query.SQL.Text:='insert into staff(name) values("Oladeji")';
   query.Open;
   query.Active:=true;
   query.Close;
